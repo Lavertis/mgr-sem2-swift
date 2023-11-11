c07efb93a2ea3c0d18bc931c3bd190a085feb09a
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2023-11-11 15:34:32 +0100

    fix(MemoryGame): hiding animation after match

--- MemoryGame/MemoryGame/CardView.swift
+++ MemoryGame/MemoryGame/CardView.swift
@@ -14,24 +14,26 @@ struct CardView: View {
         self.card = card
     }
     
+    func isShown() -> Bool {
+        return card.isFaceUp || !card.isMatched
+    }
+    
     var body: some View {
         ZStack {
             let base = RoundedRectangle(cornerRadius: 12)
             Group {
-                base.fill(.white)
                 base.strokeBorder(lineWidth: 3)
                 Text(card.content)
                     .font(.system(size: 200))
                     .minimumScaleFactor(0.01)
                     .aspectRatio(1, contentMode: .fit)
             }
-//            .opacity(card.isFaceUp ? 1 : 0)
             base
-                .fill()
                 .opacity(card.isFaceUp ? 0 : 1)
                 .animation(.easeInOut(duration: 0.5), value: card.isFaceUp)
         }
-        .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
+        .opacity(isShown() ? 1 : 0)
+        .animation(.easeInOut(duration: 0.5), value: isShown())
     }
 }
 
